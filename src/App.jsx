5abd3441dd9d566e615215f11d93644a4d8c3d58
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-12-02 17:38:46 -0300

    [add] CartItems component

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,37 @@ import {FetchData} from './components/hooks/FetchData'
 import { CardsDeals } from './components/CardsDeals/CardsDeals';
 import {SearchBar} from './components/SearchBar/SearchBar'
 import {Message} from './components/Message/Message'
+import {CartItems} from './components/CartItems/CartItems'
 
 function App() {
 
-  const steamsDeals = FetchData()
+  const fetchInitialSteamDeals = FetchData()
 
+  //states
   const [deals, setDeals] = useState([])
-
-  console.log(deals);
+  const [openCart, setOpenCart] = useState(false)
 
   useEffect(() => {
-    setDeals(() => steamsDeals )
-  }, [steamsDeals])
+    setDeals(() => fetchInitialSteamDeals )
+  }, [fetchInitialSteamDeals])
 
   const handleInput = (e) => {
     const nameOfGame = e.target.value 
-    const filteredGames = steamsDeals.filter(game => game.title.toLowerCase().includes(nameOfGame))
+    const filteredGames = fetchInitialSteamDeals.filter(game => game.title.toLowerCase().includes(nameOfGame))
     setDeals(() => filteredGames)
   }
-
-
-
   
   return (
     <div className="containerApp">
       <SearchBar handleInput={handleInput}/>
+      <CartItems/>
+      
       {deals.length === 0 ? 
         <Message/> : 
         <CardsDeals 
-        deals={deals}
+          deals={deals}
         />
       }
-      
     </div>
   );
 }
---
+++ src/components/CartItems/CartItems.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './CartItems.module.css'
+
+export const CartItems = () => {
+    return (
+        <div>
+            hola
+        </div>
+    )
+}
